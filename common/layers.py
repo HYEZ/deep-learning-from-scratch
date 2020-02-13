15586d63344287bae57d5bcf94373e371ae803f4
HYEZ <oohyejioo@gmail.com>
2020-02-13 18:40:50 +0900

    오버피팅해결(가중치감소, 드롭아웃)

--- common/layers.py
+++ common/layers.py
@@ -1,67 +1,71 @@
+# coding: utf-8
 import numpy as np
 from common.functions import *
+from common.util import im2col, col2im
+
 
 class Relu:
-	def __init__(self):
-		self.mask = None
+    def __init__(self):
+        self.mask = None
 
-	def forward(self, x):
-		self.mask = (x <= 0) # x가 0보다 작으면 Ture
-		out = x.copy() # numpy는 대입할 경우 복사X, 포인터 복사같이 주소 복사하는 개념으로 보면 될듯. copy써야 데이터만 복사함
-		out[self.mask] = 0
-		return out
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
 
-	def backward(self, dout):
-		# print(dout[self.mask])
-		
-		# print(self.mask.shape)
-		# dout[self.mask] = 0
-		dx = dout
+        return out
 
-		return dx
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
 
-# 시그모이드의 역전파는 순전파의 출력 y만으로 계산 가능하다. 
+        return dx
+
+
 class Sigmoid:
-	def __init__(self):
-		self.out = None
+    def __init__(self):
+        self.out = None
 
-	def forward(self, x):
-		out = 1 / (1 + np.exp(-x))
-		self.out = out
+    def forward(self, x):
+        out = sigmoid(x)
+        self.out = out
+        return out
 
-		return out
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
 
-	def backward(self, dout):
-		dx = dout * (1.0 - self.out) * self.out
+        return dx
 
-		return dx
 
 class Affine:
-	def __init__(self, W, b):
-		self.W = W
-		self.b = b
-		self.x = None
-		self.dW = None
-		self.db = None
-		self.original_x_shape = None
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        
+        self.x = None
+        self.original_x_shape = None
+        # 가중치와 편향 매개변수의 미분
+        self.dW = None
+        self.db = None
 
-	def forward(self, x):
-		self.x = x
-		self.original_x_shape = x.shape
-		x = x.reshape(x.shape[0], -1)
+    def forward(self, x):
+        # 텐서 대응
+        self.original_x_shape = x.shape
+        x = x.reshape(x.shape[0], -1)
+        self.x = x
 
-		out = np.dot(x, self.W) + self.b
+        out = np.dot(self.x, self.W) + self.b
 
+        return out
 
-		return out
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+        
+        dx = dx.reshape(*self.original_x_shape)  # 입력 데이터 모양 변경(텐서 대응)
+        return dx
 
-	def backward(self, dout):
-		dx = np.dot(dout, self.W.T)
-		self.dW = np.dot(self.x.T, dout)
-		self.db = np.sum(dout, axis=0)
-		dx = dx.reshape(*self.original_x_shape)
-
-		return dx
 
 class SoftmaxWithLoss:
     def __init__(self):
@@ -86,6 +90,25 @@ class SoftmaxWithLoss:
             dx = dx / batch_size
         
         return dx
+
+
+class Dropout:
+    """
+    http://arxiv.org/abs/1207.0580
+    """
+    def __init__(self, dropout_ratio=0.5):
+        self.dropout_ratio = dropout_ratio
+        self.mask = None
+
+    def forward(self, x, train_flg=True):
+        if train_flg:
+            self.mask = np.random.rand(*x.shape) > self.dropout_ratio
+            return x * self.mask
+        else:
+            return x * (1.0 - self.dropout_ratio)
+
+    def backward(self, dout):
+        return dout * self.mask
 
 
 class BatchNormalization:
@@ -170,20 +193,3 @@ class BatchNormalization:
         self.dbeta = dbeta
         
         return dx
-# test = np.array([[1, 2], [3, 4]])
-# b = np.array([1, 2])
-# print(test+b)
-
-# dy = np.array([[1, 2, 3], [4, 5, 6]])
-# db = np.sum(dy, axis=0)
-# print(db)
-
-# test = SoftmaxWithLoss()
-# a = test.forward(np.array([[1, -2], [2, -1]]), np.array([1, 0]))
-# b = test.backward()
-# print(b)
-
-# test = Sigmoid()
-# a = test.forward(np.array([[1, -2],[-1, 2]]))
-# b = test.backward(np.array([[1, -3],[-1, 2]]))
-# print(b)
